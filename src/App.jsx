e681e9153698c120ff6f5f1d26d730e5e095f663
iambinod <helloitsbinod@gmail.com>
2022-12-21 18:03:20 +0545

    added style

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,30 @@ const App = () => {
     <>
       <h1></h1>
       <form>
-        {dbArr.map((item, i) => {
-          return (
-            <>
-              <div key={i}>
-                <label>{item[0]}</label>
-                <input type={item[1].type} minLength={item[1].minLength} />
-              </div>
-            </>
-          );
-        })}
+        <div className="container">
+          <div className="form-container">
+            <div className="form-border">
+              <h3>{data.title}</h3>
+              {dbArr.map((item, i) => {
+                return (
+                  <>
+                    <div key={i} className="input-l">
+                      <label>{item[0]}</label>
+                      <input
+                        type={item[1].type}
+                        minLength={item[1].minLength}
+                      />
+                    </div>
+                  </>
+                );
+              })}
+
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
       </form>
     </>
   );
