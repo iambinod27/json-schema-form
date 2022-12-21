8a5227758381df2cd4c12644bffdc39e07d2f3fd
iambinod <helloitsbinod@gmail.com>
2022-12-21 17:36:47 +0545

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import data from "../data.json";
 
 const App = () => {
-  console.log(data);
-  return <div>App</div>;
+  const db = data.properties;
+  const dbArr = Object.entries(db);
+
+  console.log(dbArr);
+  return (
+    <>
+      <h1></h1>
+      <form>
+        {dbArr.map((item, i) => {
+          return (
+            <>
+              <div key={i}>
+                <label>{item[0]}</label>
+                <input type={item[1].type} minLength={item[1].minLength} />
+              </div>
+            </>
+          );
+        })}
+      </form>
+    </>
+  );
 };
 
 export default App;
