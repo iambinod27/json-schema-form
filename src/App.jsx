4df3cf3bd16b998d88d41d24cdec37f02b62156d
neurolinker <andha7x@gmail.com>
2022-12-24 21:06:16 +0700

    - Adding default state based on data.json properties key

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
 import data from "../data.json";
+import React from "react";
 
 const App = () => {
   const [values, setValues] = useState();
   const db = data.properties;
   const dbArr = Object.entries(db);
-
+  const field = Object.keys(data.properties)
+  React.useEffect(() => {
+    const defaultElement = {}
+    for (var key in db) {
+      const a = Object.keys(db)
+      console.log(key)
+      Object.assign(defaultElement, {[key]: '' });
+    }
+    setValues(defaultElement)
+  },[])
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
   };
 
-  const handleChange = (e) => {
+  const handleChange = (e, item) => {
     const title = e.target.title;
     const value = e.target.value;
-    setValues({ ...values, [title]: value });
+    setValues({ ...values, [item[0]]: value });
   };
 
   return (
@@ -34,7 +44,7 @@ const App = () => {
                         type={item[1].type}
                         minLength={item[1].minLength}
                         title={item[1].title}
-                        onChange={handleChange}
+                        onChange={(e) => handleChange(e,item)}
                       />
                     </div>
                   </>
