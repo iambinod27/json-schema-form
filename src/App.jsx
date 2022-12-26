a04bfd6e6b9a293f6db8daaf6a00346f28c226b3
iambinod <helloitsbinod@gmail.com>
2022-12-26 20:17:58 +0545

    form submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import data from "../data.json";
 
 const App = () => {
@@ -6,49 +6,57 @@ const App = () => {
   const db = data.properties;
   const dbArr = Object.entries(db);
 
+  useEffect(() => {
+    const defaultElement = {};
+
+    for (let key in db) {
+      console.log(key);
+      Object.assign(defaultElement, { [key]: "" });
+    }
+
+    setValues(defaultElement);
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
   };
 
-  const handleChange = (e) => {
-    const title = e.target.title;
+  const handleChange = (e, item) => {
     const value = e.target.value;
-    setValues({ ...values, [title]: value });
+    setValues({ ...values, [item[0]]: value });
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div className="container">
-          <div className="form-container">
-            <div className="form-border">
-              <h3>{data.title}</h3>
-              {dbArr.map((item, i) => {
-                return (
-                  <>
-                    <div key={i} className="input-l">
-                      <label>{item[0]}</label>
-                      <input
-                        key={i}
-                        type={item[1].type}
-                        minLength={item[1].minLength}
-                        title={item[1].title}
-                        onChange={handleChange}
-                      />
-                    </div>
-                  </>
-                );
-              })}
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+        <div className="form-container">
+          <div className="form-border">
+            <h3>{data.title}</h3>
+            {dbArr.map((item, i) => {
+              return (
+                <>
+                  <div key={i} className="input-l">
+                    <label>{item[0]}</label>
+                    <input
+                      key={i}
+                      type={item[1].type}
+                      minLength={item[1].minLength}
+                      title={item[1].title}
+                      onChange={(e) => handleChange(e, item)}
+                    />
+                  </div>
+                </>
+              );
+            })}
 
-              <button type="submit" className="btn">
-                Submit
-              </button>
-            </div>
+            <button type="submit" className="btn">
+              Submit
+            </button>
           </div>
         </div>
-      </form>
-    </>
+      </div>
+    </form>
   );
 };
 
